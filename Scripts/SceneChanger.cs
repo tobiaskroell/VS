473d908a4043dc2d36a14d2ff85e825bc4198b95
Kevin Kroell <kkroell01@gmail.com>
2023-05-27 20:13:18 +0200

    SceneChanger.cs

---
+++ Scripts/SceneChanger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneName; // The name of the scene to load, editable in the Unity editor
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
+
+
