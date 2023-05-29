41847f92a567a3c51249820d07e7bd4ba40c2338
Kevin Kroell <kkroell01@gmail.com>
2023-05-29 12:35:34 +0200

    Modify HelpBackToMain.cs

--- Scripts/HelpBackToMain.cs
+++ Scripts/HelpBackToMain.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// This is used in the HELP Scene
+
 public class HelpBackToMain : MonoBehaviour
 {
     private string previousSceneName;
