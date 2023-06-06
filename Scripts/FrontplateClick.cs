269be14feb2d7f9c590eac103c971ffaf5407225
Kevin Kroell <kkroell01@gmail.com>
2023-06-06 17:09:14 +0200

    Add FrontplateClick.cs

---
+++ Scripts/FrontplateClick.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class shows the movementInstruction gameObject when the Frontpanel is clicked
+
+public class FrontplateClick : MonoBehaviour
+{
+    private GameManager gameManager;
+    public GameObject xrayMovementInstruction;
+
+    void OnMouseDown()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        
+        if(gameManager.CurrentState == GameManager.GameState.XrayPositioning05 && PlayerPrefs.GetInt("CurrentMovementMark") == 1)
+        {
+            Debug.Log("Frontplate clicked");
+            StartCoroutine(ShowMovementInstructions());
+        }
+    }
+
+    IEnumerator ShowMovementInstructions()
+    {
+        
+        // show panel
+        xrayMovementInstruction.SetActive(true);
+
+        // wait for 5 seconds
+        yield return new WaitForSeconds(5f);
+
+        // hide panel
+        xrayMovementInstruction.SetActive(false);
+    }
+}
