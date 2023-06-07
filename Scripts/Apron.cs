dc8ffdca4e0eef4cc7732aaae02f9098f5336e0c
Kevin Kroell <kkroell01@gmail.com>
2023-06-07 20:27:48 +0200

    Change Apron.cs

--- Scripts/Apron.cs
+++ Scripts/Apron.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This class assures that the apron is only moved to the patient if 
+// we are in the correct GameState
+
 public class Apron : MonoBehaviour
 {
     // These fields can be set in the Unity Editor.
     public GameObject objectToActivate;
     public GameObject objectToDeactivate;
-    private ActionManager actionManager;
+    private GameManager gameManager;
 
     private void OnMouseDown()
     {
-        actionManager = FindObjectOfType<ActionManager>();
-        if(actionManager.CurrentState == ActionManager.GameState.RadiationProtection04)
+        gameManager = FindObjectOfType<GameManager>();
+        if (gameManager.CurrentState == GameManager.GameState.RadiationProtection06)
         {
             // Activate and deactivate the specified objects.
             objectToActivate.SetActive(true);
