dd416f8efcbb45ea5163c719225656a43fb60496
Kevin Kroell <kkroell01@gmail.com>
2023-06-10 09:59:47 +0200

    Add WidthHandler.cs

--- Scripts/WidthHandler.cs
+++ Scripts/WidthHandler.cs
@@ -9,14 +9,17 @@ public class WidthHandler : MonoBehaviour
     public GameObject right;
     public GameObject btn;
     public TextMeshProUGUI widthLabel1Text;
+    public GameObject widthPanel;
 
     public float speed = 0.01f;
     private bool isBtnClicked = false;
+    public float normalizedWidth;
 
     void Update()
     {
         float width = Vector3.Distance(left.transform.position, right.transform.position);
-        float normalizedWidth = width / 1.3f;
+
+        normalizedWidth = Mathf.Round((width / 1.2f)*100);
         widthLabel1Text.text = normalizedWidth.ToString();
 
         if (Input.GetMouseButtonDown(0))
@@ -34,9 +37,19 @@ public class WidthHandler : MonoBehaviour
 
         if (isBtnClicked)
         {
-            float scroll = Input.GetAxis("Mouse ScrollWheel");
-            left.transform.position += Vector3.right * scroll * speed;
-            right.transform.position += Vector3.left * scroll * speed;
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        left.transform.position += Vector3.right * scroll * speed;
+        right.transform.position += Vector3.left * scroll * speed;
         }
+    }
+
+    public void ActivateWidthHandler()
+    {
+        widthPanel.SetActive(true);
+    }
+
+    public void DeactivateWidthHandler()
+    {
+        widthPanel.SetActive(false);
     }
 }
