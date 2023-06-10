a2b42b662d0869928cb8b7a6f9f5c8462fd4a091
Kevin Kroell <kkroell01@gmail.com>
2023-06-10 09:59:32 +0200

    Add CheckWindow.cs

--- Scripts/CheckWindow.cs
+++ Scripts/CheckWindow.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; // for TextMeshProUGUI
 
 public class CheckWindow : MonoBehaviour
 {
@@ -9,15 +8,18 @@ public class CheckWindow : MonoBehaviour
     public GameObject bottom;
     public GameObject left;
     public GameObject right;
-    public GameObject btn;
-    public TextMeshProUGUI widthLabel1Text;
-    public TextMeshProUGUI widthLabel2Text;
+    public GameObject widthLabel1;
+    public GameObject widthLabel2;
 
-    // Distance adjustment speed
-    public float speed = 0.01f;
-
-    // To hold the state whether btn is clicked or not
-    private bool isBtnClicked = false;
+    // Specific positions to check
+    private Vector3 topMinPosition = new Vector3(-0.704f, 0.5111f, -0.1356f);
+    private Vector3 topMaxPosition = new Vector3(-0.704f, 0.5111f, -0.1456f);
+    private Vector3 bottomMinPosition = new Vector3(-0.704f, 0.5111f, 0.0447f);
+    private Vector3 bottomMaxPosition = new Vector3(0.704f, 0.5111f, 0.0347f);
+    private Vector3 leftMinPosition = new Vector3(-0.6149f, 0.5111f, -0.0444f);
+    private Vector3 leftMaxPosition = new Vector3(-0.6249f, 0.5111f, -0.0444f);
+    private Vector3 rightMinPosition = new Vector3(-0.795f, 0.5111f, -0.0444f);
+    private Vector3 rightMaxPosition = new Vector3(-0.695f, 0.5111f, -0.0444f);
 
     // Start is called before the first frame update
     void Start()
@@ -28,42 +30,52 @@ public class CheckWindow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Calculate distances
-        float width = Vector3.Distance(left.transform.position, right.transform.position);
-        float height = Vector3.Distance(bottom.transform.position, top.transform.position);
+        CheckPosition();
+    }
 
-        // Normalize distances
-        float normalizedWidth = width / 5.0f;
-        float normalizedHeight = height / 5.0f;
-
-        // Display distances in the labels
-        widthLabel1Text.text = "Width: " + width.ToString() + ", Normalized Width: " + normalizedWidth.ToString();
-        widthLabel2Text.text = "Height: " + height.ToString() + ", Normalized Height: " + normalizedHeight.ToString();
-
-        // Check if left mouse button is clicked
-        if (Input.GetMouseButtonDown(0))
+    private void CheckPosition()
+    {
+        if (IsWithinRange(top.transform.position, topMinPosition, topMaxPosition))
         {
-            // Raycast to detect clicked object
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                // If btn is clicked, toggle isBtnClicked
-                if (hit.transform.gameObject == btn)
-                {
-                    isBtnClicked = !isBtnClicked;
-                }
-            }
+            Debug.Log("Top GameObject is within its specific range.");
+        }
+        else
+        {
+            Debug.Log("Top GameObject is not within its specific range.");
         }
 
-        // If btn is clicked, adjust the distances using mouse wheel
-        if (isBtnClicked)
+        if (IsWithinRange(bottom.transform.position, bottomMinPosition, bottomMaxPosition))
         {
-            float scroll = Input.GetAxis("Mouse ScrollWheel");
-            left.transform.position += Vector3.right * scroll * speed;
-            right.transform.position += Vector3.left * scroll * speed;
-            top.transform.position += Vector3.down * scroll * speed;
-            bottom.transform.position += Vector3.up * scroll * speed;
+            Debug.Log("Bottom GameObject is within its specific range.");
         }
+        else
+        {
+            Debug.Log("Bottom GameObject is not within its specific range.");
+        }
+
+        if (IsWithinRange(left.transform.position, leftMinPosition, leftMaxPosition))
+        {
+            Debug.Log("Left GameObject is within its specific range.");
+        }
+        else
+        {
+            Debug.Log("Left GameObject is not within its specific range.");
+        }
+
+        if (IsWithinRange(right.transform.position, rightMinPosition, rightMaxPosition))
+        {
+            Debug.Log("Right GameObject is within its specific range.");
+        }
+        else
+        {
+            Debug.Log("Right GameObject is not within its specific range.");
+        }
+    }
+
+    private bool IsWithinRange(Vector3 position, Vector3 min, Vector3 max)
+    {
+        return min.x <= position.x && position.x <= max.x &&
+               min.y <= position.y && position.y <= max.y &&
+               min.z <= position.z && position.z <= max.z;
     }
 }
