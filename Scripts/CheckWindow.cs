b3f0d5603d32180bae8ae69a3774c04290c7fc07
Kevin Kroell <kkroell01@gmail.com>
2023-06-10 20:04:43 +0200

    Update CheckWindow.cs

--- Scripts/CheckWindow.cs
+++ Scripts/CheckWindow.cs
@@ -11,15 +11,20 @@ public class CheckWindow : MonoBehaviour
     public GameObject widthLabel1;
     public GameObject widthLabel2;
 
+    private bool correctPostioning_Top = false;
+    private bool correctPostioning_Bottom = false;
+    private bool correctPostioning_Left = false;
+    private bool correctPostioning_Right = false;
+
     // Specific positions to check
-    private Vector3 topMinPosition = new Vector3(-0.704f, 0.5111f, -0.1356f);
-    private Vector3 topMaxPosition = new Vector3(-0.704f, 0.5111f, -0.1456f);
-    private Vector3 bottomMinPosition = new Vector3(-0.704f, 0.5111f, 0.0447f);
-    private Vector3 bottomMaxPosition = new Vector3(0.704f, 0.5111f, 0.0347f);
-    private Vector3 leftMinPosition = new Vector3(-0.6149f, 0.5111f, -0.0444f);
-    private Vector3 leftMaxPosition = new Vector3(-0.6249f, 0.5111f, -0.0444f);
-    private Vector3 rightMinPosition = new Vector3(-0.795f, 0.5111f, -0.0444f);
-    private Vector3 rightMaxPosition = new Vector3(-0.695f, 0.5111f, -0.0444f);
+    private Vector3 topMinPosition = new Vector3(-0.704f, 0.594f, -0.17f);
+    private Vector3 topMaxPosition = new Vector3(-0.704f, 0.594f, -0.16f);
+    private Vector3 bottomMinPosition = new Vector3(-0.704f, 0.5111f, 0.028f);
+    private Vector3 bottomMaxPosition = new Vector3(0.704f, 0.5111f, 0.038f);
+    private Vector3 leftMinPosition = new Vector3(-0.58f, 0.594f, -0.0441f);
+    private Vector3 leftMaxPosition = new Vector3(-0.605f, 0.594f, -0.0441f);
+    private Vector3 rightMinPosition = new Vector3(-0.801f, 0.5111f, -0.0441f);
+    private Vector3 rightMaxPosition = new Vector3(-0.785f, 0.5111f, -0.0441f);
 
     // Start is called before the first frame update
     void Start()
@@ -27,48 +32,61 @@ public class CheckWindow : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        CheckPosition();
-    }
-
-    private void CheckPosition()
+    public bool CheckPositioning()
     {
         if (IsWithinRange(top.transform.position, topMinPosition, topMaxPosition))
         {
             Debug.Log("Top GameObject is within its specific range.");
+            correctPostioning_Top = true;
         }
         else
         {
             Debug.Log("Top GameObject is not within its specific range.");
+            correctPostioning_Top = false;
         }
 
         if (IsWithinRange(bottom.transform.position, bottomMinPosition, bottomMaxPosition))
         {
             Debug.Log("Bottom GameObject is within its specific range.");
+            correctPostioning_Bottom = true;
         }
         else
         {
             Debug.Log("Bottom GameObject is not within its specific range.");
+            correctPostioning_Bottom = false;
         }
 
         if (IsWithinRange(left.transform.position, leftMinPosition, leftMaxPosition))
         {
             Debug.Log("Left GameObject is within its specific range.");
+            correctPostioning_Left = true;
         }
         else
         {
             Debug.Log("Left GameObject is not within its specific range.");
+            correctPostioning_Left = false;
         }
 
         if (IsWithinRange(right.transform.position, rightMinPosition, rightMaxPosition))
         {
             Debug.Log("Right GameObject is within its specific range.");
+            correctPostioning_Right = true;
         }
         else
         {
             Debug.Log("Right GameObject is not within its specific range.");
+            correctPostioning_Right = false;
+        }
+
+        if (correctPostioning_Top && correctPostioning_Bottom && correctPostioning_Left && correctPostioning_Right)
+        {
+            Debug.Log("All GameObjects are within their specific range.");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Not all GameObjects are within their specific range.");
+            return false;
         }
     }
 
